fe98d3b43044bd689a200758546c5bd6ca6017ac
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-10-26 21:56:54 +0100

    second

--- day13.py
+++ day13.py
@@ -39,7 +39,7 @@ def mutate(a_list):
   b_list = []
   for item in a_list:
     new_item = item * 2
-  b_list.append(new_item)
+    b_list.append(new_item)
   print(b_list)
 
 mutate([1,2,3,5,8,13])
