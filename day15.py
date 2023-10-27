b1d0f22b019c13848e1b79472833711ebd26c207
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-10-27 14:22:40 +0100

    f

---
+++ day15.py
@@ -0,0 +1,39 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def prompt():
+    coffee_type = input("What would you like? (espresso/latte/cappuccono): ")
+    return coffee_type
+
+
+user_respnse = prompt()
