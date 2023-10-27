0bbcb293e19e3c1cd19a1ca7f3626b5747415dc1
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-10-27 13:50:14 +0100

    four

--- day14.py
+++ day14.py
@@ -1,6 +1,49 @@
+# from art import logo, vs
+# from game_data import data
+# from random import choice, randint
+# import os
+
+
+# def clear():
+#     os.system("cls")
+
+
+# print(logo)
+# updated_compare = None
+# final_score = 0
+# compare = choice(data)
+
+# while True:
+#     against = choice(data)
+#     print(
+#         f"Compare A: {compare['name']}, a {compare['description']}, from {compare['country']}.")
+#     print(vs)
+#     print(
+#         f"Against B: {against['name']}, a {against['description']}, from {against['country']}.")
+#     user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
+#     if user_ans == 'a' and compare['follower_count'] > against['follower_count']:
+#         clear()
+#         print(logo)
+#         compare = against
+#         final_score += 1
+#         print(f"You're right! Current score: {final_score}")
+#         continue
+#     elif user_ans == 'b' and against['follower_count'] > compare['follower_count']:
+#         clear()
+#         print(logo)
+#         compare = against
+#         final_score += 1
+#         print(f"You're right! Current score: {final_score}")
+#         continue
+#     else:
+#         clear()
+#         print(logo)
+#         print(f"Sorry, that's wrong. Final score: {final_score}")
+#         break
+
 from art import logo, vs
 from game_data import data
-from random import choice, randint
+from random import choice
 import os
 
 
@@ -8,31 +51,36 @@ def clear():
     os.system("cls")
 
 
-print(logo)
-updated_compare = None
-final_score = 0
+score = 0
+game_over = False
 compare = choice(data)
 
-while True:
-    against = choice(data)
-    print(
-        f"Compare A: {compare['name']}, a {compare['description']}, from {compare['country']}.")
-    print(vs)
-    print(
-        f"Against B: {against['name']}, a {against['description']}, from {against['country']}.")
-    user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
-    if user_ans == 'a' and compare['follower_count'] > against['follower_count']:
-        compare = compare
-        final_score += 1
-        print(f"You're right! Current score: {final_score}")
-        continue
-    elif user_ans == 'b' and against['follower_count'] > compare['follower_count']:
 
-        final_score += 1
-        print(f"You're right! Current score: {final_score}")
-        continue
+def display_person(person):
+    return f"{person['name']}, a {person['description']}, from {person['country']}."
+
+
+while not game_over:
+    print(logo)
+    if score > 0:
+        print(f"You're right! Current score: {score}")
+    print(f"Compare A: {display_person(compare)}")
+    print(vs)
+    against = choice(data)
+    print(f"Against B: {display_person(against)}")
+    user_ans = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    if user_ans == 'a':
+        winner = 'a' if compare['follower_count'] > against['follower_count'] else 'b'
+    else:
+        winner = 'b' if against['follower_count'] > compare['follower_count'] else 'a'
+
+    if user_ans == winner:
+        clear()
+        score += 1
+        compare = against
     else:
         clear()
         print(logo)
-        print(f"Sorry, that's wrong. Final score: {final_score}")
-        break
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_over = True
