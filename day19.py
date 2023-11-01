f453287353cf3ee236d948111b188541263a1a36
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-01 22:06:53 +0100

    19

--- day19.py
+++ day19.py
@@ -1,38 +1,39 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make you bet",
+                            prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 
-def move_forwards():
-    tim.forward(10)
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_backwards():
-    tim.backward(10)
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
 
-def move_counter_clockwise():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+            break
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-
-def move_clockwise():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
-
-
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=move_counter_clockwise)
-screen.onkey(key="d", fun=move_clockwise)
-screen.onkey(key="c", fun=clear)
 screen.exitonclick()
