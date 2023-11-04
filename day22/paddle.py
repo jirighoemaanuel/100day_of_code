6125a829acc9b0f53e96a59e79ba45e451020b64
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-04 21:10:20 +0100

    22

--- day22/paddle.py
+++ day22/paddle.py
@@ -1,20 +1,20 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
+    
     def __init__(self, position):
-
-        self.paddle = Turtle()
-        self.paddle.shape("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_len=1, stretch_wid=5)
-        self.paddle.penup()
-        self.paddle.goto(position)
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
