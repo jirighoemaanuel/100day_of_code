c8c3b36fcdbd8994cf1397a225397b601a2a95b6
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-08 21:45:21 +0100

    26

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,20 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# names = []
-# with open('./Input/Names/invited_names.txt') as invited_names:
-#     for name in invited_names:
-#         names.append(name.rstrip())
-
-# with open('./Input/Letters/starting_letter.txt', 'r') as starting_letter:
-#     letter = starting_letter.read()
-#     for name in names:
-#         letter = letter.replace["[name]", name]
-#         with open(f'./Output/ReadyToSend/{name}.txt', 'w') as readytosend:
-#             readytosend.write(letter)
 
 names = []
 with open('./Input/Names/invited_names.txt') as invited_names:
@@ -27,6 +10,3 @@ with open('./Input/Letters/starting_letter.txt', 'r') as starting_letter:
         personalized_letter = letter.replace("[name]", name)
         with open(f'./Output/ReadyToSend/{name}.txt', 'w') as readytosend:
             readytosend.write(personalized_letter)
-
-    # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-    # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
---
+++ day26/main.py
@@ -0,0 +1,4 @@
+import random
+names = ["Alex", "Beth", "Caroline", "Dave", "Elener"]
+students_socres = {student: random.randint(1, 100) for student in names}
+print(students_socres)
