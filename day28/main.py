9d01bc48922f3972b082dabef353b0a55dafae47
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-10 22:25:33 +0100

    28

--- day28/main.py
+++ day28/main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -15,13 +16,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+timer_label = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.config(fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+check_mark = Label(fg=GREEN, text="✓", bg=YELLOW)
+check_mark.grid(column=1, row=3)
+
+reset_button = Button()
+reset_button.config(text="Reset")
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
