db0a1a537deeb9e9708bd26dbf2144921ecf9e2c
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-11 17:04:21 +0100

    29

---
+++ day29/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas()
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(200, 190, image=logo_img)
+canvas.grid(column=1, row=1)
