443f69db57e606ce2e403e76d7f61a139648524a
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-21 19:56:50 +0100

    34

--- day34/ui.py
+++ day34/ui.py
@@ -1,41 +1,73 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR, padx=20, pady=20,)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        # Score Label:
-        self.score = Label()
-        self.score.config(text="Score: 0", fg="white", bg=THEME_COLOR)
-        self.score.grid(column=1, row=0)
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
 
-        # Canvas Widget
-        self.canvas = Canvas(width=300, height=250, bg='white')
-        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        # Create a white rectangle to serve as the background
-        self.canvas.create_rectangle(
-            0, 0, 300, 250, fill='white', outline='white')
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
 
-        # Create text on the canvas
-        self.question_text = self.canvas.create_text(150, 125, text="Your Question Here", font=(
-            "Arial", 20, "italic"), fill=THEME_COLOR)
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
 
-        # Buttons
-        self.true_img = PhotoImage(file='images/true.png')
-        self.true_button = Button(
-            text="Right", image=self.true_img, highlightthickness=0)
-        self.true_button.grid(column=0, row=2)
-
-        self.false_img = PhotoImage(file='images/false.png')
-        self.false_button = Button(
-            text="Wrong", image=self.false_img, highlightthickness=0)
-        self.false_button.grid(column=1, row=2)
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
