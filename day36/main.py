6eb8228aa255eeb6e871e49a05404f6d519ab1a8
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-25 10:55:49 +0100

    day36

--- day36/main.py
+++ day36/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+
 NEWS_KEY = os.getenv("NEWS_KEY")
 STOCK_KEY = os.getenv("STOCK_KEY")
 
@@ -14,36 +15,52 @@ STOCK_ENDPOINT = F"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&
 NEWS_ENDPOINT = F"https://newsapi.org/v2/everything?q={
     COMPANY_NAME}&sortBy=popularity&apiKey={NEWS_KEY}"
 
-# STEP 1: Use https://www.alphavantage.co/documentation/#daily
 
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+# Get the stock prices for tesla
 stock_response = requests.get(url=STOCK_ENDPOINT)
 stock_response.raise_for_status()
 stock_data = stock_response.json()
-yesterday_closing_price = [prices for (
-    prices) in stock_data["Time Series (Daily)"].values()][1]
-print(yesterday_closing_price['4. close'])
-# TODO 2. - Get the day before yesterday's closing stock price
+prices = [prices for (
+    prices) in stock_data["Time Series (Daily)"].values()]
 
-# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
-# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+# yesterday stock closing for tesla
+yesterday_closing_price = float(prices[0]['4. close'])
+
+# day before yesterday stock closing for tesla
+day_bfr_yesterday_closing_price = float(prices[1]['4. close'])
+# print(yesterday_closing_price, day_bfr_yesterday_closing_price)
+
+
+# Check the price difference
+price_difference = abs(yesterday_closing_price -
+                       day_bfr_yesterday_closing_price)
+price_difference = round(price_difference, 2)
+# print(price_difference)
+
+# Check the percentage difference
+percent_difference = round((price_difference/yesterday_closing_price) * 100, 2)
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
+if percent_difference > 5:
+    print('Get News')
 # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 # TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+news_response = requests.get(url=NEWS_ENDPOINT)
+news_response.raise_for_status()
+news_data = news_response.json()
 
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+first_3_articles = news_data["articles"][:3]
 
 # STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
 
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+articles_headlines_and_description = [
+    [article['title'], article['description']] for article in first_3_articles]
+print(articles_headlines_and_description)
 
 # TODO 9. - Send each article as a separate message via Twilio.
 
