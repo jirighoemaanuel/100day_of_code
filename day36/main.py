bf5d4b711ebe22c4dd85bf1fa452559ba63e36d3
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-25 11:20:04 +0100

    36

--- day36/main.py
+++ day36/main.py
@@ -1,20 +1,35 @@
 import requests
 from dotenv import load_dotenv
+from twilio.rest import Client
 import os
-load_dotenv()
 
+load_dotenv()
 
 NEWS_KEY = os.getenv("NEWS_KEY")
 STOCK_KEY = os.getenv("STOCK_KEY")
-
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["TWILIO_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+
+
+client = Client(account_sid, auth_token)
+
 
 STOCK_ENDPOINT = F"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={
     STOCK_NAME}&apikey={STOCK_KEY}"
 NEWS_ENDPOINT = F"https://newsapi.org/v2/everything?q={
     COMPANY_NAME}&sortBy=popularity&apiKey={NEWS_KEY}"
 
+news_response = requests.get(url=NEWS_ENDPOINT)
+news_response.raise_for_status()
+news_data = news_response.json()
+
+first_3_articles = news_data["articles"][:3]
 
 # Get the stock prices for tesla
 stock_response = requests.get(url=STOCK_ENDPOINT)
@@ -22,7 +37,6 @@ stock_response.raise_for_status()
 stock_data = stock_response.json()
 prices = [prices for (
     prices) in stock_data["Time Series (Daily)"].values()]
-
 
 # yesterday stock closing for tesla
 yesterday_closing_price = float(prices[0]['4. close'])
@@ -39,39 +53,21 @@ price_difference = round(price_difference, 2)
 # print(price_difference)
 
 # Check the percentage difference
-percent_difference = round((price_difference/yesterday_closing_price) * 100, 2)
+percent_difference = round(
+    (price_difference / yesterday_closing_price) * 100, 2)
 
-# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+
 if percent_difference > 5:
-    print('Get News')
-# STEP 2: https://newsapi.org/
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-news_response = requests.get(url=NEWS_ENDPOINT)
-news_response.raise_for_status()
-news_data = news_response.json()
+    articles_headlines_and_description = [
+        [article['title'], article['description']] for article in first_3_articles]
 
-first_3_articles = news_data["articles"][:3]
-
-# STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-
-
-articles_headlines_and_description = [
-    [article['title'], article['description']] for article in first_3_articles]
-print(articles_headlines_and_description)
-
-# TODO 9. - Send each article as a separate message via Twilio.
-
-
-# Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    for title, description in articles_headlines_and_description:
+        message = client.messages \
+            .create(
+                body=f"TSLA: 🔺{percent_difference}%"
+                f"Headlines: {title}"
+                f"Brief: {description}",
+                from_='+16194521087',
+                to='+2347037642158'
+            )
