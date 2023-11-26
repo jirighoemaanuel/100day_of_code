db72bfdae80a0cd77347c653b310f674ca579a9e
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-26 22:43:56 +0100

    38

--- day38/main.py
+++ day38/main.py
@@ -1,25 +1,27 @@
 import requests
 import os
+import json
 
 
 APP_ID = os.getenv('APP_ID')
 APP_KEY = os.getenv('APP_KEY')
+exercise = input("Tell me which exercises you did: ")
 
 headers = {
-    "x-app=id": APP_ID,
+    "x-app-id": APP_ID,
     "x-app-key": APP_KEY,
     "Content-Type": "application/json",
 }
 
 body = {
-    'query': "ran 1km for 20min",
-    "gender": "male",
-    "age": "22"
+    'query': exercise,
 }
+
+body_json = json.dumps(body)
 
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-response = requests.post(url=endpoint, body=body, headers=headers)
+response = requests.post(url=endpoint, data=body_json, headers=headers)
 response.raise_for_status()
 data = response.text
 print(data)
