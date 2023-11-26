755654efaa74e52dd1da92318cd9ba917daeadcf
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-26 23:41:30 +0100

    38

--- day38/main.py
+++ day38/main.py
@@ -1,27 +1,57 @@
 import requests
 import os
 import json
-
+import datetime
 
 APP_ID = os.getenv('APP_ID')
 APP_KEY = os.getenv('APP_KEY')
 exercise = input("Tell me which exercises you did: ")
+sheet_endpoint = "https://api.sheety.co/9e8f4da9e713d31f33c5798b7f6dff05/copyOfMyWorkouts/workouts"
 
-headers = {
+exercise_headers = {
     "x-app-id": APP_ID,
     "x-app-key": APP_KEY,
     "Content-Type": "application/json",
 }
 
-body = {
+sheet_headers = {
+    "Content-Type": "application/json"
+}
+
+exercise_body = {
     'query': exercise,
 }
 
-body_json = json.dumps(body)
+body_json = json.dumps(exercise_body)
 
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-response = requests.post(url=endpoint, data=body_json, headers=headers)
+response = requests.post(url=endpoint, data=body_json,
+                         headers=exercise_headers)
 response.raise_for_status()
-data = response.text
-print(data)
+data = response.json()["exercises"]
+exercise_data = [[info["duration_min"],
+                  info["nf_calories"], info["name"]] for info in data]
+
+exercise_data = []
+
+for info in data:
+    duration, calories, exercise_name = info["duration_min"], info["nf_calories"], info["name"]
+    exercise_data.append({
+        "workouts": {
+            "date": datetime.datetime.now().strftime("%d%m%Y"),
+            "time": datetime.datetime.now().strftime("%H:%M:%S"),
+            "exercise": exercise_name.title(),
+            "duration": duration,
+            "calories": calories,
+        }
+    })
+
+
+sheet_body = json.dumps(exercise_data)
+print(sheet_body)
+
+resp = requests.post(url=sheet_endpoint, data=sheet_body,
+                     headers=sheet_headers)
+resp.raise_for_status()
+print(resp.status_code)
