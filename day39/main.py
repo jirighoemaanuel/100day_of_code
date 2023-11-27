3c82f0271c2462a8b999d847f793ba1cfa78c3d4
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-11-27 20:37:14 +0100

    39

--- day39/main.py
+++ day39/main.py
@@ -1,1 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+from pprint import pprint
+
+endpoint = "https://api.sheety.co/9e8f4da9e713d31f33c5798b7f6dff05/copyOfFlightDeals/prices"
+
+response = requests.get(url=endpoint)
+response.raise_for_status()
+sheet_data = response.json()["prices"]
+
+pprint(sheet_data)
