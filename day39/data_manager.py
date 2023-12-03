bd2210eb990bac9940879467b57a41152f08aa59
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-12-03 14:40:47 +0100

    32

--- day39/data_manager.py
+++ day39/data_manager.py
@@ -1,11 +1,16 @@
-# import requests
+import requests
+import json
 
 
-# class DataManager:
-#     # This class is responsible for talking to the Google Sheet.
-#     def __init__(self, sheet_data, url, headers):
-#         self.url = url
-#         self.headers = headers
-#         self.sheet_data = sheet_data
-#         self.response = requests.put(
-#             url=self.url, data=self.sheet_data, headers=self.headers)
+class DataManager:
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self, sheet_data, url, headers):
+        self.url = url
+        self.headers = headers
+        self.sheet_data = json.dumps(sheet_data)
+        try:
+            self.response = requests.put(
+                url=self.url, data=self.sheet_data, headers=self.headers)
+            self.response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
