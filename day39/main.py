e1a62fde46da9662201271fa4870925e70ae23ec
jirighoemaanuel <emmaweyinmi@gmail.com>
2023-12-28 17:58:34 +0100

    m

--- day39/main.py
+++ day39/main.py
@@ -1,4 +1,3 @@
-# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirement
 from pprint import pprint
 from flight_search import FlightSearch
 from flight_data import FlightData
@@ -10,18 +9,4 @@ import os
 MY_EMAIL = os.getenv("EMAIL")
 MY_PASSWORD = os.getenv("PASSWORD")
 
-
-sheet = DataManager()
-sheet_data = sheet.get_sheet_data()
-for data in sheet_data:
-    flightdata = FlightData(data["iataCode"], data["city"]).check_flight_data()
-    flightdata
-    if int(data["lowestPrice"]) <= flightdata["price"]:
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(MY_EMAIL, MY_PASSWORD)
-            connection.sendmail(from_addr=MY_EMAIL,
-                                to_addrs=MY_EMAIL,
-                                msg=f"Subject:Alert! Flight Deals\n\nLow price alert! Only {flightdata['price']} to fly from {
-                                    flightdata['departure_city']}-{flightdata['departure_city_code']} to {flightdata['arrival_city']}-{flightdata['arrival_city_code']}, from {flightdata['dTime']} to {flightdata['aTime']}.")
 
---
+++ day40/notification_manager.py
@@ -0,0 +1,22 @@
+from twilio.rest import Client
+import os
+
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_VIRTUAL_NUMBER = "+16194521087"
+TWILIO_VERIFIED_NUMBER = "+2347037642158"
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
