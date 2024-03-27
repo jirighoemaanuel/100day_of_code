c9e812dcada9b3b4c245db41cd30db18d90e8798
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-03-27 23:19:55 +0100

    day57

--- day57/day57project/main.py
+++ day57/day57project/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def blog():
+    response = requests.get("https://api.npoint.io/471904fd18302b1232a6")
+    blog_data = response.json()
+    print(blog_data)
+    return render_template("index.html", blogs=blog_data)
+
+@app.route('/post/<id>')
+def post():
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
